2fce43143c763a2eb5b82571116f04cfb749ada8
Bill <60759174+BillZou123@users.noreply.github.com>
2021-06-09 22:45:07 +0800

    Update transmitter.v

--- Transmitter/transmitter.v
+++ Transmitter/transmitter.v
@@ -1,24 +1,45 @@
 
-module transmitter (clk, reset, bpsk_out, trans_out, trans_start, trans_rdy);
+
+// we play the audio at 48kHz , the audio signal is 16 bits, then we have 65 clk cycles/ bit, 5 clk cycles/sample, 13 samples/ symbol(pulse)
+module transmitter (clk, reset, bpsk_out, trans_out, trans_start);
 
 input clk, reset;
-input signed [15:0] bpsk_out;
-output reg signed [15:0] trans_out;
+input signed bpsk_out; // bpsk_out should be bit by bit
+output reg signed trans_out; // trans_out should also be bit by bit
+
+reg [6:0] counter = 0;  // this is the counter for
 
 input trans_start;
-output reg trans_rdy;
+//output reg [3:0] sample; // this is counting 13 samples / symbol(pulse)
+
+
 
 
 always@(posedge clk) begin
 
-if((!reset) && (trans_start ==1))begin
-trans_out = bpsk_out; // we are using a square pulse with 100% duty cycle here
-trans_rdy = 1;
-end
+if((!reset) && (trans_start ==1) && (counter == 0))
+ begin
+
+  trans_out = 1* bpsk_out;
+  counter = counter +1;
+  
+
+ end
+
+else if(counter < 64) 
+  begin
+    counter = counter + 1;
+   
+  end
+
+   
+    
+ 
+
 
 else begin
-trans_rdy = 0;
-trans_out = 0;
+counter = 0;
+
 end
 
 end
