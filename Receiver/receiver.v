eb05eb498b566149dc9696ee7211d803735e5a7f
Bill <60759174+BillZou123@users.noreply.github.com>
2021-06-09 22:45:37 +0800

    Update receiver.v

--- Receiver/receiver.v
+++ Receiver/receiver.v
@@ -1,1 +1,29 @@
 
+module receiver (input clk, input reset, input trans_rdy, input signed [10:0] chan_out, output reg signed receiver_out );
+
+always@(posedge clk) begin
+if((!reset) && (trans_rdy ==1)) begin
+
+  if(chan_out > 0) begin
+    receiver_out = 1;
+  end
+
+  else if (chan_out <0)begin
+    receiver_out = -1;
+  end
+  else begin
+    receiver_out = 0;
+  end
+end
+
+else begin
+
+
+receiver_out = 0;
+
+end
+end
+
+
+
+endmodule
