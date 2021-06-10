084ff6d9de21119a2e611dfdc2b9debf3e5a3113
Bill <60759174+BillZou123@users.noreply.github.com>
2021-06-10 21:27:15 +0800

    Update transmitter.v

--- Transmitter/transmitter.v
+++ Transmitter/transmitter.v
@@ -1,48 +1,78 @@
 
 
 // we play the audio at 48kHz , the audio signal is 16 bits, then we have 65 clk cycles/ bit, 5 clk cycles/sample, 13 samples/ symbol(pulse)
-module transmitter (clk, reset, bpsk_out, trans_out, trans_start);
+module transmitter (clk,reset, bpsk_out, trans_out, trans_start,trans_done);
 
-input clk, reset;
-input signed bpsk_out; // bpsk_out should be bit by bit
-output reg signed trans_out; // trans_out should also be bit by bit
+input reset,clk;
+input signed [1:0] bpsk_out; // bpsk_out should be bit by bit
+output reg signed [1:0]trans_out; // trans_out should also be bit by bit
 
-reg [6:0] counter = 0;  // this is the counter for
+
 
 input trans_start;
-//output reg [3:0] sample; // this is counting 13 samples / symbol(pulse)
+output trans_done;
 
+reg [2:0] sample_clk = 0;
+reg [3:0] sample_counter = 0;
+
+reg [2:0] state;
+parameter initial_state = 3'b000 ;
+parameter transmitting = 3'b001 ;
+parameter counting = 3'b010;
+parameter done = 3'b100;
+
+assign trans_done = state [2];
 
 
 
 always@(posedge clk) begin
 
-if((!reset) && (trans_start ==1) && (counter == 0))
- begin
-
-  trans_out = 1* bpsk_out;
-  counter = counter +1;
+if((!reset))  state = initial_state;
   
+  else begin
+   case(state)
 
- end
+     initial_state: begin               //.......................
+        trans_out = trans_out;
+        if(trans_start == 1) state = transmitting;
+        else state = initial_state;
+     end
 
-else if(counter < 64) 
-  begin
-    counter = counter + 1;
-   
+    transmitting: begin          //..................................
+       trans_out = 1* bpsk_out;
+       state = counting;
+    end
+
+    counting: begin               //......................................
+	if (sample_clk < 5) begin 
+		 sample_clk = sample_clk + 1;
+		 state = counting;
+		 end
+	else begin
+			sample_clk = 0;
+			if(sample_counter < 13) begin
+				sample_counter = sample_counter + 1;
+				state = transmitting;
+				end
+			else begin 
+				sample_counter = 0;
+				state = done;
+				end
+	end
+end
+
+
+     done: begin    //...........................
+         state = initial_state;
+     end
+        
+      
+   endcase
   end
+end
+        
 
-   
     
- 
-
-
-else begin
-counter = 0;
-
-end
-
-end
 
 
 endmodule
