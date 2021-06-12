77ccc1a8d77d9e272cc41987aa96e80a0dafaac8
Bill <60759174+BillZou123@users.noreply.github.com>
2021-06-12 19:00:04 +0800

    Update transmitter.v

--- Transmitter/transmitter.v
+++ Transmitter/transmitter.v
@@ -1,6 +1,6 @@
 
 
-// we play the audio at 48kHz , the audio signal is 16 bits, then we have 65 clk cycles/ bit, 5 clk cycles/sample, 13 samples/ symbol(pulse)
+// we play the audio at 48kHz , the audio signal is 16 bits, then we have 65 clk cycles/ bit, 5 clk cycles/sample, 12 samples/ symbol(pulse)
 module transmitter (clk,reset, bpsk_out, trans_out, trans_start,trans_done);
 
 input reset,clk;
@@ -10,7 +10,7 @@ output reg signed [1:0]trans_out; // trans_out should also be bit by bit
 
 
 input trans_start;
-output trans_done;
+output reg trans_done;
 
 reg [2:0] sample_clk = 0;
 reg [3:0] sample_counter = 0;
@@ -21,7 +21,7 @@ parameter transmitting = 3'b001 ;
 parameter counting = 3'b010;
 parameter done = 3'b100;
 
-assign trans_done = state [2];
+//assign trans_done = state [2];
 
 
 
@@ -34,29 +34,36 @@ if((!reset))  state = initial_state;
 
      initial_state: begin               //.......................
         trans_out = trans_out;
+        trans_done = 0;
         if(trans_start == 1) state = transmitting;
         else state = initial_state;
      end
 
     transmitting: begin          //..................................
        trans_out = 1* bpsk_out;
+       trans_done = 1;
        state = counting;
     end
 
-    counting: begin               //......................................
-	if (sample_clk < 5) begin 
+    counting: begin 
+                    //......................................
+	if (sample_clk < 3) begin 
 		 sample_clk = sample_clk + 1;
+                 
 		 state = counting;
 		 end
+        
 	else begin
 			sample_clk = 0;
-			if(sample_counter < 13) begin
+			if(sample_counter < 11) begin
 				sample_counter = sample_counter + 1;
 				state = transmitting;
 				end
 			else begin 
 				sample_counter = 0;
 				state = done;
+                                trans_out = 0;
+                                trans_done = 0;
 				end
 	end
 end
